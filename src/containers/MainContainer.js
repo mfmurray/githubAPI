9345da7239e9d79fc67db968721a8dff7bfa1ca5
MichaelMurray <mfmurray@umich.edu>
2020-12-13 16:53:17 -0500

    Added dark-content status bar
    time and icons in the status bar are more visible

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -17,6 +17,9 @@ const MainContainer = (props) => {
         addingCommits={addingCommits}
         addingCommitsError={addingCommitsError}
       />
+      <StatusBar
+        barStyle="dark-content"
+      />
     </SafeAreaView>
   );
 }
