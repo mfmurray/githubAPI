7514fe6d7a7bebdbadb59a4f850205ff8ebe343e
MichaelMurray <mfmurray@umich.edu>
2020-12-13 17:25:13 -0500

    Author, message, and hash is visble on eash List Item

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -8,14 +8,51 @@ const ListItem = (props) => {
   var filteredMessage = filterMessage(props.message)
 
   return (
-  <View>
-    <Text style={styles.textAuthor}>{props.author}</Text>
+  <View style={styles.main}>
+    <View style={styles.container}>
+      <Text style={styles.textMessage}>{filteredMessage[0]}</Text>
+      <Text style={styles.textHash}> - {filteredMessage[2]}</Text>
+      <Text style={styles.textAuthor}> {props.author}</Text>
+      <Text style={styles.textHash}>{props.hash}</Text>
+    </View>
   </View>
   );
 };
 
 
 const styles = StyleSheet.create({
+  textMessage: {
+    fontSize:15,
+    fontWeight:'bold'
+  },
+  textAuthor: {
+    fontSize:15,
+  },
+  textHash: {
+    marginTop:15,
+    fontSize:15,
+  },
+  container: {
+    width: Dimensions.get('window').width*.95,
+    backgroundColor: "white",
+    marginTop: Dimensions.get('window').width*.0125,
+    marginBottom: Dimensions.get('window').width*.0125,
+    padding:5,
+    borderRadius:5,
+
+      shadowColor: "#000",
+      shadowOffset: {
+	       width: 0,
+	       height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 3.84,
+      elevation: 5,
+  },
+  main: {
+    width: Dimensions.get('window').width,
+    alignItems:'center'
+  },
 });
 
 
--- src/redux/api/commits.js
+++ src/redux/api/commits.js
@@ -2,7 +2,7 @@
 
 export async function getCommits() {
   console.log('get commits api')
-    const response = await fetch(`https://api.github.com/repos/mfmurray/test/commits`, {
+    const response = await fetch(`https://api.github.com/repos/mfmurray/githubAPI/commits`, {
         method: 'GET',
         headers: {'Content-Type': 'application/json'},
       })
