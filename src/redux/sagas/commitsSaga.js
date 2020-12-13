981f57b6f0dba8a3a00e833d6165740bbfb48fb6
MichaelMurray <mfmurray@umich.edu>
2020-12-13 16:26:02 -0500

    Added Redux Sagas
    Added sagas Redux and a watcher to call an API

---
+++ src/redux/sagas/commitsSaga.js
@@ -0,0 +1,8 @@
+import { call, put, takeEvery, takeLatest, all } from 'redux-saga/effects'
+
+
+
+export function* commitsSaga() {
+
+  console.log("get API")
+}
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,12 @@
+// Imports: Dependencies
 import AsyncStorage from '@react-native-community/async-storage';
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
+import createSagaMiddleware from 'redux-saga';
 import { logger } from 'redux-logger';
-
+// Imports: Redux
 import rootReducer from './reducers';
+import rootSaga from './sagas'
 
 const persistConfig = {
   // Root?
@@ -21,10 +24,13 @@ const persistConfig = {
 
 // Middleware: Redux Persist Persisted Reducer
 const persistedReducer = persistReducer(persistConfig, rootReducer)
+const sagaMiddleware = createSagaMiddleware();
 // Redux: Store
 const store = createStore(
   persistedReducer,
+  applyMiddleware(sagaMiddleware),
 );
+sagaMiddleware.run(rootSaga)
 
 // Middleware: Redux Persist Persister
 let persistor = persistStore(store);
