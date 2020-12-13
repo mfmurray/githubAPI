e4979f4e65aac98e65f7c6a455d51c8f2d89631b
MichaelMurray <mfmurray@umich.edu>
2020-12-13 17:04:17 -0500

    filter fetched commit data
    Sort data by Author, message, and hash, and urls

--- src/components/ListScreen.js
+++ src/components/ListScreen.js
@@ -2,15 +2,23 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, Image, StyleSheet, TouchableOpacity, FlatList } from "react-native";
 import { useDispatch, useSelector  } from 'react-redux';
 
+import {filterCommits} from '../helpers/filterCommits'
 
 const ListScreen = (props) => {
   const {addingCommitsDone, addingCommits, addingCommitsError} = props;
 
   const dispatch = useDispatch();
+  const [data, setData] = useState(null);
 
   useEffect(() => {
     dispatch({ type: 'ADD_COMMITS'})
   }, []);
+
+  useEffect(() => {
+    if (addingCommitsDone) {
+      setData(filterCommits(addingCommitsDone))
+    }
+  }, [addingCommitsDone]);
 
   return (
     <View>
---
+++ src/helpers/filterCommits.js
@@ -0,0 +1,21 @@
+
+
+
+
+export function filterCommits(commitsData) {
+
+  var data=[]; var obj
+  commitsData.forEach((arr, index) => {
+      obj = {}
+      obj.author = arr.author.login
+      obj.authorUrl = arr.author.html_url
+      obj.iconUrl = arr.author.avatar_url
+      obj.message = arr.commit.message
+      obj.hash = arr.sha
+      obj.hashUrl = arr.html_url
+      data.push(obj)
+  })
+
+  return data
+
+}
